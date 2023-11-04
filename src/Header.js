8c5058807df71bf2ab19b221f57300039eba250a
Onkar <onkarpune2016@gmail.com>
2023-11-04 20:34:12 +0530

    Header added

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+const Header = function(){
+    return(
+        <div className="header">
+            Phone Directory
+            </div>
+
+    )
+}
+export default Header;
