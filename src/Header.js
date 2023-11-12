d3522c48d1dafb91c6b7e06f0f9c0e58743196b5
Onkar <onkarpune2016@gmail.com>
2023-11-12 15:04:19 +0530

    Passed heading to Header component as props

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,28 @@
 import React, {Component} from "react";
 import './Header.css';
-const Header = function(){
+
+{/* Functional */}
+{/*const Header = function(props){
     return(
         <div className="header">
-            Phone Directory
+            {props.heading}
             </div>
 
     )
 }
+export default Header;
+*/}
+
+{/*Class */}
+
+class Header extends Component{
+    render(){
+        return (
+            <div className="header">
+            {this.props.heading}
+            </div>
+       );
+    }
+}
+
 export default Header;
